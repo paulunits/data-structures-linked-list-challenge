1dd88d2caa258de22e85a03c181f1c9126afd1b9
paulunits <obsolete.units@gmail.com>
2016-08-09 15:49:25 -0700

    Add linked list and node structures

--- linked_list.rb
+++ linked_list.rb
@@ -2,4 +2,27 @@ require_relative 'node'
 
 class LinkedList
 
+	attr_reader :first, :last
+
+	def initialize
+		@first = nil
+		@last = nil
+	end	
+
+	def insert_first(element)
+		@first = element
+	end
+	
+	def remove_first
+		@first = nil
+	end
+	
+	def insert_last(element)
+		@last = element
+	end
+	
+	def remove_last
+		@last = nil
+	end					
+
 end
--- node.rb
+++ node.rb
@@ -1,3 +1,17 @@
 class Node
 
+	attr_reader :element, :next
+
+	def initialize(element)
+		@element = element
+		@next = nil
+	end
+	
+	def insert_after(other_node)	
+		@next = other_node
+	end	
+
+	def remove_after
+		@next = nil
+	end	
 end
